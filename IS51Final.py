dfe23c05b3bc8534c5abfb00aa51e95a400b82f0
Pheng <phengyangster9@gmail.com>
2022-05-19 16:17:01 -0700

    structured-english completed

---
+++ IS51Final.py
@@ -0,0 +1,15 @@
+"""
+The program is trying to display multiple outputs including
+the number of grades, the average grade, and the percentages of grades
+that are above the average grade.
+First initialize the function to display the number of grades. 
+Secondly, write a calulate_percentage_above_average() function to
+calculate the percentage of grades that are above the average grade.
+
+Function 1 will kickstart the application, it will extract from
+the Final.txt file and calculate the average grade and close after the 
+information has been collected.
+Function 2 named percentage of grades above average will calculate 
+the percentage of grades that are above the average grade.
+"""
+
